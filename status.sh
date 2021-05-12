ce46cc15db3658393a9f5632b325bf5980d00ddb
manfly17 <280368847@qq.com>
2021-05-12 11:00:27 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -65,7 +65,7 @@ check_pid_client(){
 }
 Download_Server_Status_server(){
 	cd "/tmp"
-	wget -N --no-check-certificate "https://ghproxy.com/https://github.com/manfly17/ServerStatus-Toyo/archive/master.zip"
+	wget -N --no-check-certificate "https://mirror.ghproxy.com/https://github.com/manfly17/ServerStatus-Toyo/archive/master.zip"
 	[[ ! -e "master.zip" ]] && echo -e "${Error} ServerStatus 服务端下载失败 !" && exit 1
 	unzip master.zip
 	rm -rf master.zip
@@ -100,7 +100,7 @@ Download_Server_Status_server(){
 }
 Download_Server_Status_client(){
 	cd "/tmp"
-	wget -N --no-check-certificate "https://ghproxy.com/https://raw.githubusercontent.com/manfly17/ServerStatus-Toyo/master/clients/status-client.py"
+	wget -N --no-check-certificate "https://mirror.ghproxy.com/https://raw.githubusercontent.com/manfly17/ServerStatus-Toyo/master/clients/status-client.py"
 	[[ ! -e "status-client.py" ]] && echo -e "${Error} ServerStatus 客户端下载失败 !" && exit 1
 	cd "${file_1}"
 	[[ ! -e "${file}" ]] && mkdir "${file}"
@@ -127,14 +127,14 @@ Download_Server_Status_client(){
 }
 Service_Server_Status_server(){
 	if [[ ${release} = "centos" ]]; then
-		if ! wget --no-check-certificate "https://ghproxy.com/https://raw.githubusercontent.com/manfly17/doubi/master/service/server_status_server_centos" -O /etc/init.d/status-server; then
+		if ! wget --no-check-certificate "https://mirror.ghproxy.com/https://raw.githubusercontent.com/manfly17/doubi/master/service/server_status_server_centos" -O /etc/init.d/status-server; then
 			echo -e "${Error} ServerStatus 服务端服务管理脚本下载失败 !" && exit 1
 		fi
 		chmod +x /etc/init.d/status-server
 		chkconfig --add status-server
 		chkconfig status-server on
 	else
-		if ! wget --no-check-certificate "https://ghproxy.com/https://raw.githubusercontent.com/manfly17/doubi/master/service/server_status_server_debian" -O /etc/init.d/status-server; then
+		if ! wget --no-check-certificate "https://mirror.ghproxy.com/https://raw.githubusercontent.com/manfly17/doubi/master/service/server_status_server_debian" -O /etc/init.d/status-server; then
 			echo -e "${Error} ServerStatus 服务端服务管理脚本下载失败 !" && exit 1
 		fi
 		chmod +x /etc/init.d/status-server
@@ -144,14 +144,14 @@ Service_Server_Status_server(){
 }
 Service_Server_Status_client(){
 	if [[ ${release} = "centos" ]]; then
-		if ! wget --no-check-certificate "https://ghproxy.com/https://raw.githubusercontent.com/manfly17/doubi/master/service/server_status_client_centos" -O /etc/init.d/status-client; then
+		if ! wget --no-check-certificate "https://mirror.ghproxy.com/https://raw.githubusercontent.com/manfly17/doubi/master/service/server_status_client_centos" -O /etc/init.d/status-client; then
 			echo -e "${Error} ServerStatus 客户端服务管理脚本下载失败 !" && exit 1
 		fi
 		chmod +x /etc/init.d/status-client
 		chkconfig --add status-client
 		chkconfig status-client on
 	else
-		if ! wget --no-check-certificate "https://ghproxy.com/https://raw.githubusercontent.com/manfly17/doubi/master/service/server_status_client_debian" -O /etc/init.d/status-client; then
+		if ! wget --no-check-certificate "https://mirror.ghproxy.com/https://raw.githubusercontent.com/manfly17/doubi/master/service/server_status_client_debian" -O /etc/init.d/status-client; then
 			echo -e "${Error} ServerStatus 客户端服务管理脚本下载失败 !" && exit 1
 		fi
 		chmod +x /etc/init.d/status-client
@@ -626,9 +626,9 @@ Modify_config_client(){
 Install_jq(){
 	if [[ ! -e ${jq_file} ]]; then
 		if [[ ${bit} = "x86_64" ]]; then
-			wget --no-check-certificate "https://ghproxy.com/https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64" -O ${jq_file}
+			wget --no-check-certificate "https://mirror.ghproxy.com/https://github.com/manfly17/jq/releases/download/jq-1.6/jq-linux64" -O ${jq_file}
 		else
-			wget --no-check-certificate "https://ghproxy.com/https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux32" -O ${jq_file}
+			wget --no-check-certificate "https://mirror.ghproxy.com/https://github.com/manfly17/jq/releases/download/jq-1.6/jq-linux32" -O ${jq_file}
 		fi
 		[[ ! -e ${jq_file} ]] && echo -e "${Error} JQ解析器 下载失败，请检查 !" && exit 1
 		chmod +x ${jq_file}
@@ -646,7 +646,7 @@ Install_caddy(){
 		Set_server "server"
 		Set_server_http_port
 		if [[ ! -e "/usr/local/caddy/caddy" ]]; then
-			wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/manfly17/ServerStatus-Toyo/master/caddy_install.sh
+			wget -N --no-check-certificate https://mirror.ghproxy.com/https://raw.githubusercontent.com/manfly17/ServerStatus-Toyo/master/caddy_install.sh
 			chmod +x caddy_install.sh
 			bash caddy_install.sh install
 			rm -rf caddy_install.sh
@@ -776,7 +776,7 @@ Uninstall_ServerStatus_server(){
 		rm -rf "/etc/init.d/status-server"
 		if [[ -e "/etc/init.d/caddy" ]]; then
 			/etc/init.d/caddy stop
-			wget -N --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/manfly17/ServerStatus-Toyo/master/caddy_install.sh
+			wget -N --no-check-certificate https://mirror.ghproxy.com/https://raw.githubusercontent.com/manfly17/ServerStatus-Toyo/master/caddy_install.sh
 			chmod +x caddy_install.sh
 			bash caddy_install.sh uninstall
 			rm -rf caddy_install.sh
